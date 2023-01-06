29d2ed617aaee5aa218b511bc2af09bbc89ae0fe
Jared Crawford <jmcrawford45@gmail.com>
2023-01-06 01:28:33 -0600

    p72

--- 72.py
+++ 72.py
@@ -1,13 +1,33 @@
-from math import ceil
-from utils import gcd
-def count_range(n) -> int:
-    count = 0
-    for denominator in range(2, n + 1):
-        if denominator % 100 == 0:
-            print(denominator)
-        for numerator in range(ceil(denominator/3), ceil(denominator/2)+1):
-            if gcd(numerator, denominator) == 1 and 1/3 < numerator / denominator < 1/2:
-                count += 1
-    return count
-assert count_range(8) == 3
-print(count_range(12_000))
+from utils import *
+from collections import defaultdict
+
+# @lru_cache(maxsize=None)
+# def phi(n: int) -> int:
+#     res = n
+#     for p in primes:
+#         if p > n:
+#             break
+#         if n % p == 0:
+#             res -= res // p
+#         while n % p == 0:
+#             n //= p
+#     if n > 1:
+#         res -= res // n
+#     return res
+
+def reduced_fractions_below(d: int) -> int:
+    phi = list(range(d+1))
+    for i, v in enumerate(phi):
+        if i < 2:
+            continue
+        if i == v:
+            for j in range(i, len(phi), i):
+                phi[j] //= i
+                phi[j] *= i-1
+    res = 0
+    for i in range(2, d+1):
+        if i % 10000 == 0:
+            print(f"{i//10000}%")
+    return sum(phi[2:])
+assert reduced_fractions_below(8) == 21
+print(reduced_fractions_below(1_000_000))
