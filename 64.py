edcc6b08da55c0cc75b51a54b08a95c3d17e6376
Jared Crawford <jmcrawford45@gmail.com>
2023-01-06 21:53:02 -0600

    p64

--- 64.py
+++ 64.py
@@ -5,28 +5,32 @@ def get_period(n):
     cycle = []
     seen = set()
     a = floor(sqrt(n))
-    prev = None
-    denom = sqrt(n)-a
-    while (a, prev) not in seen:
-        seen.add((a, prev))
-        prev = a
-        cycle.append(a)
-        a = floor(1/(denom))
-        denom = (1/denom-a)
-    print(n, cycle)
-    cycle = cycle[1:]
-    if cycle[0] == cycle[-1] and len(cycle) >= 2:
-        cycle = cycle[:-1]
-    return cycle
-print(get_period(23))
+    numerator = 1
+    denom_sub = a
+    while (a, numerator, denom_sub) not in seen:
+        """
+        a0 = 4, 1/(sqrt(23)-4) = 
+        """
+        seen.add((a, numerator, denom_sub))
+        cycle.append((a, numerator, denom_sub))
+        tmp_denom = (n-denom_sub**2)//numerator
+        tmp_num_add = denom_sub
+        a = floor((sqrt(n)+tmp_num_add)/tmp_denom)
+        numerator = tmp_denom
+        denom_sub = -(tmp_num_add - a*(tmp_denom))
+    cycle = cycle[cycle.index((a, numerator, denom_sub)):]
+    return [c[0] for c in cycle]
+
+assert get_period(1) == []
+assert get_period(2) == [2]
 assert get_period(23) == [1,3,1,8]
+assert get_period(13) == [1,1,1,1,6]
 
 def odd_periods_below(n) -> int:
     count = 0
     for i in range(2, n+1):
-        print(i, get_period(n))
-        # if len(get_period(n)) % 2 == 1:
-        #     count += 1
+        if len(get_period(i)) % 2 == 1:
+            count += 1
     return count
-print(odd_periods_below(13))
-# assert odd_periods_below(13) == 4
+assert odd_periods_below(13) == 4
+print(odd_periods_below(10_000))
