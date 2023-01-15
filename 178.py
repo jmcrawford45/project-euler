f33eae1c16effb22f0a9e3d24f001c734bc4279d
Jared Crawford <jmcrawford45@gmail.com>
2023-01-14 20:01:56 -0600

    p178

--- 178.py
+++ 178.py
@@ -2,19 +2,23 @@ from functools import lru_cache
 
 @lru_cache(maxsize=None)
 def _count_steps(digits: int, prev: int, used) -> int:
-    if digits == 0 :
+    if digits == 0:
         return int(all(used))
     res = 0
-    if prev < len(used):
-        res += _count_steps(digits - 1, prev + 1, used[:prev] + (True,) + used[prev+1:])
+    if prev < 9:
+        new_used = list(used)
+        new_used[prev+1] = True
+        res += _count_steps(digits - 1, prev + 1, tuple(new_used))
     if prev > 0:
-        res += _count_steps(digits - 1, prev - 1, used[:prev-1] + (True,) + used[prev:])
+        new_used = list(used)
+        new_used[prev-1] = True
+        res += _count_steps(digits - 1, prev - 1, tuple(new_used))
     return res
 
 def count_steps(digits: int, usable: range) -> int:
-    used = (False,) * (len(usable)+1)
+    used = (False,) * (10)
     res = 0
-    for in_len in range(len(usable), digits+1):
+    for in_len in range(len(used), digits+1):
         res += sum(_count_steps(in_len-1, i, used[:i] + (True,) + used[i+1:]) for i in usable)
     return res
 print(count_steps(40, range(1, 10)))
